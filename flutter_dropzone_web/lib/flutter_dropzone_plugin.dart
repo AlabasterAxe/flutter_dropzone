70838616e5bde2f3ccca2f55e734c708ddaa9af9
Matthew Keller <matthew.ed.keller@gmail.com>
2025-11-16 07:23:14 -0500

    Replace CircularProgressIndicator with SizedBox

--- flutter_dropzone_web/lib/flutter_dropzone_plugin.dart
+++ flutter_dropzone_web/lib/flutter_dropzone_plugin.dart
@@ -162,7 +162,7 @@ class FlutterDropzonePlugin extends FlutterDropzonePlatform {
           } else if (snapshot.hasError)
             return const Center(child: Text('Error loading library'));
           else
-            return const Center(child: CircularProgressIndicator());
+            return const SizedBox();
         },
       );
 }
